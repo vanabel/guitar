14f70502651df13ba4ae26c00a2bb410e0c3710e
Van Abel <van141.abel@gmail.comgmail.com>
2019-05-03 21:51:22 +0800

    add picnicss style

--- src/App.vue
+++ src/App.vue
@@ -194,7 +194,7 @@ export default {
 }
 </script>
 <style>
-/* @import "../node_modules/picnic/picnic.min.css"; */
+@import "../node_modules/picnic/picnic.min.css";
 #app{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
